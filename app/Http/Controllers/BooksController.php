73b65a26fe43c9a37fabc8c6c4503b9dcd5a7599
yennnting <mssylviee@gmail.com>
2021-04-27 17:20:17 +0800

    adds the Book resource

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    public function store()
+    {
+        Book::create($this->validateRequest());
+    }
+
+    public function update(Book $book)
+    {
+        $book->update($this->validateRequest());
+    }
+
+    /**
+     * @return array
+     */
+    protected function validateRequest(): array
+    {
+        return request()->validate([
+            'title' => 'required',
+            'author' => 'required',
+        ]);
+    }
+}
